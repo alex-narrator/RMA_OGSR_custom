f49100470e991d0f77657c15fe919a5452d1d72e
xer-urg <alex.narrator@gmail.com>
2025-02-03 17:05:18 +0200

    +анімація огляду артефакту з Artifacts Inspection

--- gamedata/scripts/personal_test.lua
+++ gamedata/scripts/personal_test.lua
@@ -109,7 +109,7 @@ end
 	-- main_wnd.show_zone_map = not main_wnd.show_zone_map
 -- end
 
-game.play_hud_motion(2, "heal_wounded_hud", "anm_use", false, 1, true)
+game.play_hud_motion(2, "artefact_inspect_hud", "anm_show", false, 1, true)
 -- level.add_cam_effector('camera_effects\\kick\\attack_heavy.anm', 8073, false,'')
 --game.stop_hud_motion()
 --actor:activate_slot(NO_ACTIVE_SLOT, true)
